81ef76768b64086d1c812c53006d7ac960b5c0a1
Matthew Barulic <matthew.barulic@gmail.com>
2023-06-19 21:48:37 -0400

    Fixes error in top level script dir discovery

--- coach_computer_setup.sh
+++ coach_computer_setup.sh
@@ -4,27 +4,27 @@
 # intended to run on a machine with a fresh install of Ubuntu. It will install
 # all dependencies and clone and build our software repo.
 
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SRC_SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/src
 
 source src/color_utils.sh
 
 # Make sure we're in the user's home directory
 cd $HOME
 
-source $SCRIPT_DIR/src/enable_sudo_actions.sh
+source $SRC_SCRIPT_DIR/enable_sudo_actions.sh
 
-source $SCRIPT_DIR/src/error_trap_utils.sh
+source $SRC_SCRIPT_DIR/error_trap_utils.sh
 
 #
 # Configuration steps
 #
-$SCRIPT_DIR/src/upgrade_apt_packages.sh
-$SCRIPT_DIR/src/install_common_tools.sh
-$SCRIPT_DIR/src/install_league_docker_images.sh
-$SCRIPT_DIR/src/install_ssl_quality_inspector.sh
-$SCRIPT_DIR/src/setup_software_repo.sh
-$SCRIPT_DIR/src/setup_firmware_repo.sh
-$SCRIPT_DIR/src/add_ros_to_bash_profile.sh
+$SRC_SCRIPT_DIR/upgrade_apt_packages.sh
+$SRC_SCRIPT_DIR/install_common_tools.sh
+$SRC_SCRIPT_DIR/install_league_docker_images.sh
+$SRC_SCRIPT_DIR/install_ssl_quality_inspector.sh
+$SRC_SCRIPT_DIR/setup_software_repo.sh
+$SRC_SCRIPT_DIR/setup_firmware_repo.sh
+$SRC_SCRIPT_DIR/add_ros_to_bash_profile.sh
 
 echo -e "\n\n${GREEN}System configuration complete!${NC}"
 exit 0
